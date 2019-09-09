a79a1dbdf96334953a778f2b3e7106105c04518a
Codrut Alexandru <panea_codrut@yahoo.com>
2019-09-09 23:25:26 +0300

    much progress

--- server_test.py
+++ server_test.py
@@ -1,7 +1,7 @@
 import time
 import json
 from flask import Flask, make_response
-
+import threading
 
 DEVICE_ID = 1
 SURVEILLANCE_AREA = "Calea Victoriei nr. xyz"
@@ -23,7 +23,24 @@ def hello_world():
 
 	apiResponse = make_response( jsonResponse )
 	apiResponse.mimetype = 'application/json'
+
 	return apiResponse
+
+jsonResponse = json.dumps({
+        "ID dispozitiv": DEVICE_ID,
+        "Locatia supravegheata": SURVEILLANCE_AREA,
+        "Pista ocupata": isLaneOccupied,
+        "Timp ocupare pista": occupiedTimeElapsed
+        })
+
+# with open('data.json', 'w', encoding='utf-8') as f:
+#     json.dump(jsonResponse, f, ensure_ascii=False, indent=4)
+
+    
+with open('data.json', encoding='utf-8') as f:
+    datele = json.load(f)
+    print(datele)
+
 
 
 app.run()
--- vehicle_detection.py
+++ vehicle_detection.py
@@ -76,6 +76,7 @@ while True:
     # Detects cars of different sizes in the input image 
     cars = car_cascade.detectMultiScale(gray, 1.1, 1) 
     
+    # Measure time spent on track
     if type(cars) is np.ndarray and isLastFrameDetected == False:
         start = time.time()
         isLastFrameDetected = True
@@ -86,6 +87,32 @@ while True:
             occupiedTimeElapsed = elapsedTimeOnTrack
             isLastFrameDetected = False
 
+    if elapsedTimeOnTrack > 30 and isLaneOccupied = False:
+        isLaneOccupied = True
+
+        jsonResponse = json.dumps({
+        "ID dispozitiv": DEVICE_ID,
+        "Locatia supravegheata": SURVEILLANCE_AREA,
+        "Pista ocupata": isLaneOccupied,
+        "Timp ocupare pista": occupiedTimeElapsed
+        })
+
+        with open('data.json', 'w', encoding='utf-8') as f:
+            json.dump(jsonResponse, f, ensure_ascii=False, indent=4)
+
+    elif isLaneOccupied = True and elapsedTimeOnTrack < 30
+        isLaneOccupied = False
+
+        jsonResponse = json.dumps({
+        "ID dispozitiv": DEVICE_ID,
+        "Locatia supravegheata": SURVEILLANCE_AREA,
+        "Pista ocupata": isLaneOccupied,
+        "Timp ocupare pista": occupiedTimeElapsed
+        })
+
+        with open('data.json', 'w', encoding='utf-8') as f:
+            json.dump(jsonResponse, f, ensure_ascii=False, indent=4)
+            
     # To draw a rectangle in each cars 
     for (x,y,w,h) in cars: 
         cv2.rectangle(image,(x,y),(x+w,y+h),(0,0,255),2) 
